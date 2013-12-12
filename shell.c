ec25288c8dc25dc9e13acd8f35f79878a9189b25
evalincius <ed@oa-edu-176-149.wireless.abdn.ac.uk>
2013-12-12 20:28:05 +0000

    comment on shell.c also

--- shell.c
+++ shell.c
@@ -10,26 +10,24 @@ int main()
    
    Byte text[4*BLOCKSIZE] = "ABaaaBABASad asdasasdas asdasdasdasdasd";
    MyFILE * file;
-   file = myfopen("testfile.txt", "w");
-   
-   for(int i =0; i<4*BLOCKSIZE; i++)
+   file = myfopen("testfile.txt", "w"); // opens virtual file
+   for(int i =0; i<4*BLOCKSIZE; i++) 
    {
-   	Byte byte = text[i];
-   	//printf("file's pos %d\n", text[i]);
-   	myfputc ( byte, file );
+   	Byte byte = text[i]; // iterates through text which is content to put to file
+   	myfputc ( byte, file ); // puts byte to file.
    }
-    myfclose(file); 	
+    myfclose(file); 	// close file.
  //---------GET------------------------------------
    
    MyFILE * file2;
-   FILE *fp;
-   fp=fopen("file.txt","w");
-   file2 = myfopen("testfile.txt", "r");
+   FILE *fp; 
+   fp=fopen("file.txt","w"); // opens real file
+   file2 = myfopen("testfile.txt", "r"); // opens virtual file
    for(int i =0; i<4*BLOCKSIZE; i++){
-      Byte data = myfgetc(file2);
-      putc(data,fp);
+      Byte data = myfgetc(file2); //gets data byte prom file
+      putc(data,fp); //puts it to real file 
    } 
-   fclose(fp);
+   fclose(fp); // close real file
    //---------------------------------------------
    printf("Writing disk\n");
    writedisk("virtualdisk12_14");
